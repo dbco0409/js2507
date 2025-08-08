d01bf3a773448d9c92e01f2b25a98cd34e843872
dbco0409 <75134167+dbco0409@users.noreply.github.com>
2025-08-08 16:46:02 +0900

    uu

---
+++ work/js09_0808/Garvarge.jsx
@@ -0,0 +1,13 @@
+import Car from "./Car"
+
+function Garvarge(){
+  return(
+    <>
+      <h1>내 차고에는 어떤 차가 있을까?</h1>
+      <Car color="yellow" num="1" />
+      <Car color="yellow" num="2" />
+      <Car color="yellow" num="3" />
+    </>
+  )
+}
+export default Garvarge
---
+++ work/js09_0808/car.jsx
@@ -0,0 +1,6 @@
+function Car(pr){
+  return(
+    <h2>안녕.. 빵빵이의 색상은 (pr.color)</h2>
+  )
+}
+export default Car;
